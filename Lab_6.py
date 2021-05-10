b809d893515077d6fe3f88e1184d0dda0e1e1cc5
Xavier Nunez <xaviernunez83@gmail.com>
2021-05-10 09:04:36 -0700

    first commit

---
+++ Lab_6.py
@@ -0,0 +1,32 @@
+
+
+
+
+"""
+To statr, we will generate a random interger between 1 and 20, and 
+based on the result of the number, we check to see if it falls under certain range
+if the number is greater than 15, then the result will be "Cherries"
+otherwise if the number is > 10, then the result wil be "Orange"
+otherwise if the number is > 10, then the result wil be "Plum"
+otherwise if the number is > 10, then the result wil be "Melon"
+otherwise if the number is > 10, then the result wil be "Bell"
+if the number is not any of the above, then the reult will be "Bar"
+
+we iterate over using a loop three times ab=nd print the reults to the user. An example "Plum Cherries"
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
